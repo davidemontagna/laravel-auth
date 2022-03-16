b5b6082dc761b7491b67389b1d9d5980c26b39af
davidemontagna <davidemontagna92@gmail.com>
2022-03-16 17:41:07 +0100

    inserito bottone per tornare alla index

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -18,7 +18,10 @@
                             <li class="list-group-item">Content: {{$post->content}}</li>
                             <li class="list-group-item">Slug: {{$post->slug}}</li>                        
                         </ul>
+                        <div class="card-body">
+                            <a href="{{route("admin.posts.index")}}" class="card-link"><button type="button" class="btn btn-info">Back to table</button></a>
                         
+                        </div>
                     </div>
                 </div>
             </div>
