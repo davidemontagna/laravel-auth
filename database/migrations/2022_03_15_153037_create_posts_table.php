5138595fbd1d39ff159a94141be54908d2b0c07d
davidemontagna <davidemontagna92@gmail.com>
2022-03-16 15:37:49 +0100

    inserimento dashboard

--- database/migrations/2022_03_15_153037_create_posts_table.php
+++ database/migrations/2022_03_15_153037_create_posts_table.php
@@ -17,7 +17,7 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->string('title', 150);
             $table->text('content');
-            $table->boolean('published')->default(false);
+            // $table->boolean('published')->default(false);
             $table->string('slug')->unique();
             $table->timestamps();
         });
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,5 +3,45 @@
 @section('title', 'Post')
     
 @section('content')
-    
+
+<section>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">title</th>
+            <th scope="col">content</th>
+            <th scope="col">thumb</th>
+            
+        </tr>
+        </thead>
+        <tbody>
+
+            @foreach ($posts as $post)
+
+        <tr>          
+            <th scope="row">{{$post->id}}</th>
+            <td>{{$post->title}}</td>
+            <td>{{$post->content}}</td>
+            <td>{{$comic->type}}</td>
+            <td>
+                <a href="{{route("posts.show", $post->id)}}"><button type="button" class="btn btn-info">Show</button></a>
+                <a href="{{route("posts.edit", $post->id)}}"><button type="button" class="mt-2 btn btn-success">Edit</button></a>
+                <form action="{{route("posts.destroy", $post->id)}}" method="POST">
+                @csrf
+                @method("DELETE")
+                
+                    <input class="mt-2 btn btn-danger" type="submit" value="Delete">
+                </form>
+            </td>
+           
+        </tr>
+
+        @endforeach
+        
+        </tbody>
+    </table>
+    <a href="{{route("posts.create", $post->id)}}"><button type="button" class="mb-5 btn btn-dark">Inserisci nuovo</button></a>
+</section>
+
 @endsection
